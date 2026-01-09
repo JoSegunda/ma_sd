520673456d0454abeb0ee1bc412e2056e7cc9429
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-09 14:52:01 +0000

    adicionar estrutura para o admin

---
+++ admin-cli/src/main/java/pt/admin/admin_cli/AdminCliApplication.java
@@ -0,0 +1,13 @@
+package pt.admin.admin_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminCliApplication.class, args);
+	}
+
+}
---
+++ admin-cli/src/test/java/pt/admin/admin_cli/AdminCliApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.admin.admin_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
