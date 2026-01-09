8295950216c10add1b6a80bad96015c4dec09db4
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-09 14:44:36 +0000

    adicionar estrutura para o server

--- server/src/test/java/pt/sd/server/ServerApplicationTests.java
+++ server/src/test/java/pt/sd/server/ServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.server.demo;
+package pt.sd.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
