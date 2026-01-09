646db9fb47d6373d6d7ec9e19f2380f172733720
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-09 14:52:34 +0000

    adicionar estrutura para o client-rest

---
+++ client-rest/src/test/java/pt/client/client_rest/ClientRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.client.client_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
