ade9d03420646dee4b7563f860f0a7ceea66c66a
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-18 15:22:41 +0000

    adds gerador client grpc

--- client-grpc/src/main/java/pt/client_gRPC/client_gRPC/ClientGRpcApplication.java
+++ client-grpc/src/main/java/pt/client_gRPC/client_gRPC/ClientGRpcApplication.java
@@ -1,13 +1,46 @@
 package pt.client_gRPC.client_gRPC;
 
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pt.sd.server.grpc.*; // Gerado pelo plugin 
 
 @SpringBootApplication
-public class ClientGRpcApplication {
+public class ClientGRpcApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientGRpcApplication.class, args);
-	}
+    @GrpcClient("server-grpc") // Configurado no application.properties
+    private MetricasServiceGrpc.MetricasServiceBlockingStub stub;
 
+    public static void main(String[] args) {
+        SpringApplication.run(ClientGRpcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        GeradorSimulado gerador = new GeradorSimulado();
+        String idDispositivo = "GATEWAY_UEVORA_01"; // Deve estar registado na BD [cite: 96]
+
+        while (true) {
+            gerador.gerarNovosDados();
+
+            // Criar o pedido binário forte [cite: 38, 39]
+            PedidoMetrica pedido = PedidoMetrica.newBuilder()
+                    .setIdDispositivo(idDispositivo)
+                    .setTemperatura(gerador.getTemperatura())
+                    .setHumidade(gerador.getHumidade())
+                    .setTempo(gerador.getTimestampAtual())
+                    .build();
+
+            try {
+                // Chamada síncrona [cite: 37, 45]
+                RespostaMetrica resposta = stub.enviarMetrica(pedido);
+                System.out.println("gRPC: " + resposta.getMensagem());
+            } catch (Exception e) {
+                System.err.println("Erro na ligação gRPC: " + e.getMessage());
+            }
+
+            Thread.sleep(5000); // Envia a cada 5 segundos [cite: 43]
+        }
+    }
 }
