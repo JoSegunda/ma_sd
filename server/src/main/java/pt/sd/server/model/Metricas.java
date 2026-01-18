8e5e71ebbb38748c5998ea4a6753193665da3c78
Joaquim Segunda <joelpereirasegunda@gmail.com>
2026-01-18 14:05:51 +0000

    adicionar atributos a metricas

---
+++ server/src/main/java/pt/sd/server/model/Metricas.java
@@ -0,0 +1,50 @@
+package pt.sd.server.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+public class Metricas {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String idDispositivo;
+    private float temperatura;
+    private float humidade;
+    private LocalDateTime tempo;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getIdDispositivo() {
+        return idDispositivo;
+    }
+    public void setIdDispositivo(String idDispositivo) {
+        this.idDispositivo = idDispositivo;
+    }
+    public float getTemperatura() {
+        return temperatura;
+    }
+    public void setTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+    public float getHumidade() {
+        return humidade;
+    }
+    public void setHumidade(float humidade) {
+        this.humidade = humidade;
+    }
+    public LocalDateTime getTempo() {
+        return tempo;
+    }
+    public void setTempo(LocalDateTime tempo) {
+        this.tempo = tempo;
+    }
+
+    
+}
